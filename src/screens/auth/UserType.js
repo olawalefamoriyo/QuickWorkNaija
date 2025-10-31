5391920e1655dd9d6031103a13ec4fd8a4d8fe28
olawalefamoriyo <famjohn11@gmail.com>
2025-10-31 10:22:54 +0100

    Update UserType.js

--- src/screens/auth/UserType.js
+++ src/screens/auth/UserType.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
